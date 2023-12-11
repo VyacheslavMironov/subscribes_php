cae2b3e9672b22afc8c72415fb22d11d438d092d
VuacheslavMironov <vuacheslav.mir@gmail.com>
2023-12-12 00:34:37 +0700

    Добавил роутинг

--- src/public/emit_log_topic.php
+++ src/public/emit_log_topic.php
@@ -7,19 +7,18 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('rabbitmq', 5672, 'brooks_sub', 'babka123');
 $channel = $connection->channel();
 
-$channel->exchange_declare('direct_logs', 'direct', false, false, false);
+$channel->exchange_declare('topic_logs', 'topic', false, false, false);
 
-$severity = $argv[1];
-if(empty($severity)) $severity = "info";
-
+$routing_key = $argv[1];
+if(empty($routing_key)) $routing_key = "anonymous.info";
 $data = implode(' ', array_slice($argv, 2));
 if(empty($data)) $data = "Hello World!";
 
 $msg = new AMQPMessage($data);
 
-$channel->basic_publish($msg, 'direct_logs', $severity);
+$channel->basic_publish($msg, 'topic_logs', $routing_key);
 
-echo " [x] Sent ",$severity,':',$data," \n";
+echo " [x] Sent ",$routing_key,':',$data," \n";
 
 $channel->close();
 $connection->close();
